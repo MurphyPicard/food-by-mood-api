7cd29caac901739073058edf2a7b5ac8ed07c4f3
murphypicard <ayaghsizian@gmail.com>
2017-01-23 12:15:32 -0500

    changed name to title in index js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/api/foods", function(req, res){
   });
 });
 
-app.get("/api/foods/:name", function(req, res){
+app.get("/api/foods/:title", function(req, res){
   Food.findOne({name: req.params.name}).then(function(food){
     res.json(food);
   });
@@ -33,13 +33,13 @@ app.post("/api/foods", function(req, res){
   });
 });
 
-app.delete("/api/foods/:name", function(req, res){
+app.delete("/api/foods/:title", function(req, res){
   Food.findOneAndRemove({name: req.params.name}).then(function(){
     res.json({success: true });
   });
 });
 
-app.put("/api/candidates/:name", function(req, res){
+app.put("/api/candidates/:title", function(req, res){
   Food.findOneAndUpdate({name: req.params.name}, req.body, {new: true}).then(function(food){
     res.json(food);
   });
